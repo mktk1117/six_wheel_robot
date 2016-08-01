0969b1319e45ce5fe96f217a8c173e852a7ec711
Takahiro Miki <takahiro.miki1992@gmail.com>
2016-08-01 11:46:47 +0900

    add some comments

--- ROS/motor_driver/scripts/motor_driver.py
+++ ROS/motor_driver/scripts/motor_driver.py
@@ -3,7 +3,6 @@
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
-from numpy import*
 import serial
 
 
@@ -12,25 +11,27 @@ class driver:
         # init ros
         rospy.init_node('car_driver', anonymous=True)
         rospy.Subscriber('/cmd_vel', Twist, self.get_cmd_vel)
-        # self.pub_cmd_vel = rospy.Publisher('cmd_vel',Twist,queue_size=20)
         self.ser = serial.Serial('/dev/ttyUSB0', 115200)
         self.get_arduino_message()
 
+    # get cmd_vel message, and get linear velocity and angular velocity
     def get_cmd_vel(self, data):
         x = data.linear.x
         angular = data.angular.z
         self.send_cmd_to_arduino(x, angular)
 
+    # translate x, and angular velocity to PWM signal of each wheels, and send to arduino
     def send_cmd_to_arduino(self, x, angular):
+        # calculate right and left wheels' signal
         right = int((x + angular) * 50)
         left = int((x - angular) * 50)
+        # format for arduino
         message = "{},{},{},{},{},{}*".format(right, left, right, left, right, left)
         print message
-        self.send_arduino_message(message)
-
-    def send_arduino_message(self, message):
+        # send by serial 
         self.ser.write(message)
 
+    # receive serial text from arduino and publish it to '/arduino' message
     def get_arduino_message(self):
         pub = rospy.Publisher('arduino', String, queue_size=10)
         r = rospy.Rate(10)
