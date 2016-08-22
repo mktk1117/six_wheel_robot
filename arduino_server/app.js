88aabee153fdb4dc1432576544e5a5b1976d9dcc
Takahiro Miki <takahiro.miki1992@gmail.com>
2016-08-22 18:45:36 +0900

    Add keyboard control and start stop button

--- arduino_server/app.js
+++ arduino_server/app.js
@@ -1,32 +1,15 @@
 var http = require('http');
 var socketio = require('socket.io');
 var fs = require('fs');
+var path = require('path')
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type' : 'text/html'});
     res.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
 }).listen(3000); 
 
-var serialPort = require("serialport")
-var sp = new serialPort.SerialPort("/dev/ttyUSB0", {
-  baudrate: 115200,
-  parser:serialPort.parsers.readline("\n")
-});
-
-// sp.on("open", function () {
-//   console.log('open');
-//   setTimeout(function() {
-//     sp.write("Hello "); 
-//     sp.write("world\n");
-//     console.log('Sended msg'); 
-//   }, 2000); 
-// });
-
-  sp.on('data', function(data) {
-  console.log('data received: ' + data);
-});
- 
 var io = socketio.listen(server);
 
+var auto_control = false;
 
 // connection
 io.sockets.on('connection', function(socket) {
@@ -43,35 +26,58 @@ io.sockets.on('connection', function(socket) {
 
     // recieve cmd_vel
     socket.on('cmd_vel', function(data) {
+        // S06. server_to_clientイベント・データを送信する
         var linear = data.linear;
         var angular = data.angular;
         var right = (linear.x + angular.z) * 50;
         var left = (linear.x - angular.z) * 50;
         var command = right + "," + left + "," + right + "," + left + "," + right + "," + left + "*";
+        if(auto_control == true){
+            io.to("remotecontrol").emit('cmd_vel', data);
+            // sp.write(command);
+        }
+        else if(data.from == "remotecontrol"){
+            io.to("remotecontrol").emit('cmd_vel', data);
+            // sp.write(command);
+        }
+        else{
+            var command = "0,0,0,0,0,0*";
+            // sp.write(command);
+        }
 
-        // send cmd_vel to remotecontrol client
-        io.to("remotecontrol").emit('cmd_vel', data);
-        // send command to Arduino
-        sp.write(command);
+
     });
 
-    // transfer data to other room
+    socket.on('startstop', function(data) {
+        var startstop_flag = data.flag;
+        if(startstop_flag == "start"){
+            auto_control = true;
+        }
+        else{
+            auto_control = false;
+            var command = "0,0,0,0,0,0*";
+            // sp.write(command);
+        }
+    });
+
+    // recieve cmd_vel
     socket.on('transfer', function(data) {
+        // S06. server_to_clientイベント・データを送信する
         var transfer_data = data.data;
         var dstroom = data.room;
         var eventname = data.event;
         io.to(dstroom).emit(eventname, {data : transfer_data});
     });
 
-    // transfer messaget to other room
     socket.on('transfer_message', function(data) {
+        // S06. server_to_clientイベント・データを送信する
         var dstroom = data.room;
         var eventname = data.event;
         var message = data.message
         io.to(dstroom).emit(eventname, {message : message});
     });
 
-    // receive disconnection
+    // S09. dicconnectイベントを受信し、退出メッセージを送信する
     socket.on('disconnect', function() {
         if (room == '') {
             console.log("disconnected without entering a room");
